a411a2f0b6649e2643480086512158d29a2d26d8
Maripo GODA <avenafatua@gmail.com>
2012-12-20 21:49:55 +0900

    Add dropdown menu

--- src/main/java/org/jenkinsci/plugins/blink1/Blink1Notifier.java
+++ src/main/java/org/jenkinsci/plugins/blink1/Blink1Notifier.java
@@ -32,6 +32,7 @@ public class Blink1Notifier extends Notifier {
 	private static final String COLOR_YELLOW = "FFFF00";
 	private static final String COLOR_RED = "FF0000";
 	
+	private static final String DEFAULT_BLINK_INTERFACE = "webApi";
 	private static final String DEFAULT_URL_BASE = "http://localhost:8934";
 	private static final String DEFAULT_COMMAND_PATH = "/usr/bin/blink1-tool";
 	
@@ -40,6 +41,7 @@ public class Blink1Notifier extends Notifier {
 	@Override
 	public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
 		listener.getLogger().println("Blink1Notifier.perform");
+		listener.getLogger().println("descriptor.interface=" + getDescriptor().getBlinkInterface());
 		BallColor color = build.getResult().color;
 		String colorCode = "FFFFFF";
 		if (BallColor.BLUE==color)
@@ -54,6 +56,7 @@ public class Blink1Notifier extends Notifier {
 
 	private void blink(BuildListener listener, String colorCode) {
 		String urlStr = getDescriptor().getUrlBase() + "/blink1/fadeToRGB?rgb=%23" + colorCode + "&time=" + DELAY;
+		
 		URL url;
 		try {
 			URLConnection conn;
@@ -81,14 +84,18 @@ public class Blink1Notifier extends Notifier {
 		return (DescriptorImpl)super.getDescriptor();
 	}
 	
+	private static final String FORM_KEY_BLINK_INTERFACE = "blinkInterface";
 	private static final String FORM_KEY_URL_BASE = "urlBase";
 	private static final String FORM_KEY_COMMAND_PATH = "commandPath";
+	private static final String FORM_VALUE_BLINK_INTERFACE_WEB_API = "webApi";
+	private static final String FORM_VALUE_BLINK_INTERFACE_COMMANDLINE = "commandline";
 	
 	@Extension
 	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
 		private String urlBase = DEFAULT_URL_BASE;
 		private String commandPath = DEFAULT_COMMAND_PATH;
+		private String blinkInterface = DEFAULT_BLINK_INTERFACE;
 		
 		public DescriptorImpl() {
 			load();
@@ -99,6 +106,9 @@ public class Blink1Notifier extends Notifier {
 		}
 		public String defaultCommandPath() {
 			return DEFAULT_COMMAND_PATH;
+		}
+		public String defaultBlinkInterface  () {
+			return DEFAULT_BLINK_INTERFACE;
 		}
 		
 		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
@@ -111,6 +121,7 @@ public class Blink1Notifier extends Notifier {
 				return false;
 			this.urlBase = form.getString(FORM_KEY_URL_BASE);
 			this.commandPath = form.getString(FORM_KEY_COMMAND_PATH);
+			this.blinkInterface = form.getString(FORM_KEY_BLINK_INTERFACE);
 			save();
 			return super.configure(req, form);
 		}
@@ -128,12 +139,22 @@ public class Blink1Notifier extends Notifier {
 		public String getDisplayName() {
 			return "Blink1Notifier";
 		}
+
+		public boolean isBlinkInterfaceWebApi () {
+			return FORM_VALUE_BLINK_INTERFACE_WEB_API.equals(blinkInterface);
+		}
+		public boolean isBlinkInterfaceCommandline () {
+			return FORM_VALUE_BLINK_INTERFACE_COMMANDLINE.equals(blinkInterface);
+		}
 		
 		public String getUrlBase () {
 			return this.urlBase;
 		}
 		public String getCommandPath () {
 			return this.commandPath;
+		}
+		public String getBlinkInterface () {
+			return this.blinkInterface;
 		}
 	}
 }
